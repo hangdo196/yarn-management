840f256affdc5a0a98d92f88087236bad125c418
hangdo196 <dohang190603@gmail.com>
2025-04-13 20:52:31 +0200

    test

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
+import { RegisterOwnerComponent } from './register-owner/register-owner.component';
 
 export const routes: Routes = [
-    { path: 'register', component: RegisterComponent },
+    { path: 'register-User', component: RegisterComponent },
+    { path: 'register-Owner', component: RegisterOwnerComponent}
 ];
---
+++ frontend/src/app/register-owner/register-owner.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-owner',
+  imports: [],
+  templateUrl: './register-owner.component.html',
+  styleUrl: './register-owner.component.css'
+})
+export class RegisterOwnerComponent {
+
+}
